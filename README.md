1bdc72e8ffbfb99ee0cd57f272879c38adbce6cd
zhibi <1017866168@qq.com>
2019-09-23 17:31:41 +0800

    add: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## picgo-plugin-smms-user>
+
+使用SM.MS V2的API上传，适用于注册了SM.MS的用户。需要填写`Authorization`
---
+++ src/index.js
@@ -0,0 +1,80 @@
+module.exports = (ctx) => {
+  const register = () => {
+    ctx.helper.uploader.register('smms-user', {
+      handle,
+      name: 'SM.MS-登录用户',
+      config: config
+    })
+  }
+  const postOptions = (Authorization, fileName, image) => {
+    return {
+      method: 'POST',
+      url: `https://sm.ms/api/v2/upload`,
+      headers: {
+        contentType: 'multipart/form-data',
+        'Authorization': Authorization||undefined,
+      },
+      formData: {
+        smfile: {
+          value: image,
+          options: {
+            filename: fileName
+          }
+        }
+      }
+    };
+  };
+  const handle = async (ctx) => {
+    let userConfig = ctx.getConfig('picBed.smms-user')
+    if (!userConfig) {
+      throw new Error('Can\'t find uploader config')
+    }
+    const Authorization = userConfig.Authorization
+    const imgList = ctx.output;
+    for (let i in imgList) {
+      let image = imgList[i].buffer;
+      if (!image && imgList[i].base64Image) {
+        image = Buffer.from(imgList[i].base64Image, 'base64');
+      }
+      const postConfig = postOptions(Authorization, imgList[i].fileName, image);
+      let body = await ctx.Request.request(postConfig);
+      
+      body = JSON.parse(body);
+      if (body.code === 'success') {
+        delete imgList[i].base64Image;
+        delete imgList[i].buffer;
+        imgList[i]['imgUrl'] = body.data.url;
+      }
+      else {
+        ctx.emit('notification', {
+          title: '上传失败',
+          body: body.msg
+        });
+        throw new Error(body.msg);
+      }
+    }
+    return ctx;
+  };
+  
+  const config = ctx => {
+    let userConfig = ctx.getConfig('picBed.smms-user')
+    if (!userConfig) {
+      userConfig = {}
+    }
+    return [
+      {
+        name: 'Authorization',
+        type: 'input',
+        default: userConfig.Authorization,
+        required: true,
+        message: 'Auth',
+        alias: 'Auth'
+      }
+    ]
+  }
+  return {
+    uploader: 'smms-user',
+    config: config,
+    register
+  }
+}
